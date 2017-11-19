cb9d6667defb7e31174d8d697a9e9b6f97a6051e
Charlie Escoboza Garcia <ing.charlie.garcia@gmail.com>
2017-11-19 16:34:48 -0400

    Add educational section style (#3)

--- lib/app/views/components/Sections/EducationSection.jsx
+++ lib/app/views/components/Sections/EducationSection.jsx
@@ -3,26 +3,46 @@
  */
 
 import * as _ from 'lodash';
+import classnames from 'classnames';
 import React from 'react';
 import Mosaic from '../Mosaic.jsx';
 import SectionTitle from '../SectionTitle.jsx';
 
 
-const getContent = (item, status) => {
-  return (
-    <div className="info">
-      <p className="start-date">{`Start date: ${item.startDate}`}</p>
-      <p className="start-date">{`End date: ${item.endDate}`}</p>
-      <p className="status">{status}</p>
-    </div>
-  );
+class Content extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+    this._openInfoSection = this._openInfoSection.bind(this);
+  }
+
+  _openInfoSection() {
+    this.setState({
+      open: !this.state.open
+    });
+  }
+
+  render() {
+    const { item } = this.props;
+    const status = item.endDate ? 'Finish' : 'In Progress';
+
+    return (
+      <div className={classnames("info", {'open': this.state.open})}>
+        <button className="collapsible-button" onClick={this._openInfoSection}>Details</button>
+        <p className="start-date">{`Start date: ${item.startDate}`}</p>
+        <p className="start-date">{`End date: ${item.endDate}`}</p>
+        <p className="status">{`Status: ${status}`}</p>
+      </div>
+    );
+  }
 };
 
 
 const prerenderItem = (item) => {
-  const status = item.endDate ? 'Finish' : 'In Progress';
-
-  return _.assign({}, item, {content: getContent(item, status)});
+  return _.assign({}, item, {content: <Content item={item} />});
 };
 
 
@@ -34,7 +54,7 @@ const EducationSection = ({courses, sectionLink}) => {
   const items = courses.map(prerenderItem);
 
   return (
-    <div id={sectionLink} className="testimonial-section section" >
+    <div id={sectionLink} className="educational-section section" >
       <SectionTitle disableUrl={true} title="Education" />
       <Mosaic items={items} theme="education" />
     </div>
